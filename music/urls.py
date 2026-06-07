8eb73536bcf9f23894fbae764e7400c0d925ac70
Samuel <diego.moreno2004sm@gmail.com>
2026-06-07 00:33:19 -0600

    feat: implementar logout y mejorar el header

--- music/urls.py
+++ music/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('search/', views.search, name='search'),
+    path('logout/', views.logout_view, name='logout'),
 ]
