a3f17ba36bbb6ce4c3b34d90600d76f9b3d2ddf8
Samuel <diego.moreno2004sm@gmail.com>
2026-06-06 19:23:29 -0600

    feat: inicio de sesion

--- music/urls.py
+++ music/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
     path('search/', views.search, name='search'),
 ]
