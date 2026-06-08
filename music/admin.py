4083f0390367e3755c7b508359ae04864e860371
Samuel <diego.moreno2004sm@gmail.com>
2026-06-07 18:39:01 -0600

    fix: agregar modelo UserProfile al panel de admin

--- music/admin.py
+++ music/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Artist, Album, Song, Playlist
+from .models import Artist, Album, Song, Playlist, UserProfile
 
 admin.site.register(Artist)
 admin.site.register(Album)
 admin.site.register(Song)
 admin.site.register(Playlist)
+admin.site.register(UserProfile)
